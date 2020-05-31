d82d5bd93bba5f96d194a7a9d8dcb560f13d5f12
Andrei Riazanov <Apple556@yandex.ru>
2020-05-31 16:31:50 +0400

    Modify Account info

--- src/pages/Account/index.js
+++ src/pages/Account/index.js
@@ -7,10 +7,14 @@ import {
 import { getAccountInfo } from '../../services/accountApi'
 import RegistrationForm from '../../forms/RegistrationForm'
 import LoginForm from '../../forms/LoginForm'
-import AccountInfo from '../../components/AccountInfo';
 import LogoutForm from '../../forms/LogoutForm';
-import BasePage from '../../components/BasePage'
-import Section from '../../components/Section';
+import MainColumn from '../../components/MainColumn';
+import Row from 'react-bootstrap/Row';
+import RightColumn from '../../components/RightColumn';
+import Col from 'react-bootstrap/Col';
+import { isAuthenticated } from '../../services/authApi'
+import UserCard from '../../components/UserCard'
+import AdvertisingCard from '../../components/AdvertisingCard'
 
 function Account(props) {
   const [data, setData] = React.useState([])
@@ -23,11 +27,51 @@ function Account(props) {
   }, [])
 
   return (
-    <BasePage title="Аккаунт">
-      <Section title="Вход">
-
-      </Section>
-      <main>
+    <Row>
+      <MainColumn>
+        <Row>
+          <Col>
+            {
+              isAuthenticated() ? <h1>Мой профиль</h1> : <h1>Войти</h1>
+            }
+          </Col>
+          <Col>
+            {
+              !isAuthenticated() && <h1>Регистрация</h1>
+            }
+          </Col>
+        </Row>
+        {
+          isAuthenticated()
+          ? <>
+          <Row>
+            <Col xs={12} md={12} lg={isAuthenticated() ? 6 : 12} xl={isAuthenticated() ? 6 : 12} >
+              {
+                !!account && <h3>Вы вошли в систему, как { account.username }</h3>
+              }
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={12} md={12} lg={isAuthenticated() ? 6 : 12} xl={isAuthenticated() ? 3 : 12} >
+              <LogoutForm />
+            </Col>
+          </Row>
+          </>
+          : <Row>
+            <Col>
+              {
+                <LoginForm />
+              }
+            </Col>
+            <Col>
+              {
+                <RegistrationForm />
+              }
+            </Col>
+          </Row>
+        }
+        
+      {/* <main>
         <nav className="additional">
           <Link to="/account">Аккаунт</Link>
           <Link to="/account/login" disabled>Войти</Link>
@@ -64,8 +108,13 @@ function Account(props) {
             }
           </Route>
         </Switch>
-      </main>
-    </BasePage>
+      </main> */}
+      </MainColumn>
+      <RightColumn>
+        <UserCard />
+        <AdvertisingCard />
+      </RightColumn>
+    </Row>
   )
 }
 
